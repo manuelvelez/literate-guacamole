880160b44921ed1ebf4e4e15ede7b2e4b10e0f3f
Manuel Vélez Martínez <manuelvelez@gmail.com>
2023-03-03 08:55:59 +0100

    initial commit

--- generator.sh
+++ generator.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if (( $RANDOM % 3 == 0 ))
+if (( $RANDOM % 2 == 0 ))
 then
 	DIR=$(date +%Y%m)
 	FILE=$(date +%d)
 	mkdir -p $DIR
 	echo "whatever" >> $DIR/$FILE
 	git add $DIR/$FILE
-	git commit -m "new commi in $DIR/FILE"
+	git commit -m "new commit in $DIR/FILE"
 	git push origin main
 else
 	DATE=$(date +%F%T)
